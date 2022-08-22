36ca88689c53c1af6c2346a81d2c89b5b5c9337f
Ahmad Bakare <65125857+adelekeahmad@users.noreply.github.com>
2022-08-22 02:10:15 -0700

    Add project files.

---
+++ ForLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ahmadForLoop;
+class Program
+{
+    static void Main(string[] arg)
+    {
+      int a = 1;
+        int[] luckyNumbers = { 10, 15, 20, 25, 30, 40}; 
+       // while(i < 5) set i loop condition
+        //{
+          //  Console.WriteLine(i);
+            //i++;  increment i by 1
+        //}
+        for(a =1; a<=7; a++)
+        {
+            Console.WriteLine(a);
+        }
+        for(int i = 0; i < luckyNumbers.Length; i++) 
+        {
+            Console.WriteLine(luckyNumbers[i]);
+        }
+        Console.ReadLine();
+    }
+}
